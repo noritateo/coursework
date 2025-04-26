90607af17d630c0d54bdffb0f8ccced43066d9d7
NoritaTeo <40739778@live.napier.ac.uk>
2025-04-26 22:30:00 +0800

    added timer

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const audio = document.getElementById('audio-player');
+const showInfo = document.getElementById('show-info');
 
 var artists = [
     "Adele", "Coldplay", "Bruno Mars", "Taylor Swift", 
     "Lady Gaga", "The Weeknd", "Billie Eilish", "Imagine Dragons", 
     "Rihanna", "Post Malone"
-  ];
+];
   
 let currentTrack = 0;
+let countdownTimer;
 
 function playSong() {
-    var correctArtist = artists[currentTrack % 10];
-    var url = "https://itunes.apple.com/search?term=" + encodeURIComponent(correctArtist) + "&media=music&entity=song&limit=10";
+  var correctArtist = artists[currentTrack % 10];
+  var url = "https://itunes.apple.com/search?term=" + encodeURIComponent(correctArtist) + "&media=music&entity=song&limit=10";
   
-    fetch(url)
-    .then(function(response) {
-      return response.json();
+  fetch(url)
+  .then(function(response) {
+    return response.json();
     })
     .then(function(data) {
       var song = data.results[0];
@@ -24,8 +26,30 @@ function playSong() {
       audio.play();
 
       currentTrack++;  
+
+      startCountdown();
+
     });
 }
 
+function startCountdown() {
+
+  let counter = 5; 
+  showInfo.textContent = counter;
+
+  countdownTimer = setInterval(function() {
+    counter--;
+    if (counter > 0) {
+      showInfo.textContent = counter;
+    } else {
+      clearInterval(countdownTimer);
+      showInfo.textContent = "Game over!";
+
+      setTimeout(function() {
+        playSong(); 
+      }, 5000);
+    }
+  }, 1000);
+}
 
 playSong();
